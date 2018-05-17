622f8d8c14b30de1202e7554aafb2c1592b701c7
Nikita <basal.nikita35@gmail.com>
2018-05-17 11:34:12 +0530

    commit nikita test

--- app/src/main/java/sfatechgroup/inventory/testActivity.java
+++ app/src/main/java/sfatechgroup/inventory/testActivity.java
@@ -1,4 +1,14 @@
 package sfatechgroup.inventory;
 
-public class testActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class testActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supply_history);
+
+    }
 }
